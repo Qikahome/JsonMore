aa153001e31abbb8f8ea3b6385d62d2dce25ff0a
奇卡之家 <1828665870@qq.com>
2026-04-17 12:24:08 +0800

    1.0.6

--- src/main/java/qikahome/jsonmore/JsonMore.java
+++ src/main/java/qikahome/jsonmore/JsonMore.java
@@ -4,11 +4,16 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
+import dev.gigaherz.jsonthings.things.ThingRegistries;
+import dev.gigaherz.jsonthings.things.parsers.ThingResourceManager;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.core.Registry;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -18,11 +23,14 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.resource.ResourcePackLoader;
 import qikahome.jsonmore.cyclopscore.CyclopsCorePlugin;
 import qikahome.jsonmore.cyclopscore.ScrollingContainerScreen;
+import qikahome.jsonmore.lib.ContainerPart;
 import qikahome.jsonmore.minecraft.MinecraftPlugin;
 import qikahome.jsonmore.musbox.AnvilMusBoxPlugin;
 import qikahome.jsonmore.tconstruct.TConstructPlugin;
@@ -55,6 +63,8 @@ public class JsonMore {
 
         // 注册我们mod的ForgeConfigSpec，以便Forge可以为我们创建和加载配置文件
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        onFlexTypesLoad();
     }
 
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister
@@ -101,6 +111,7 @@ public class JsonMore {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             event.enqueueWork(() -> {
+                Registry.register(ThingRegistries.PROPERTIES, "jsonmore:container_part", ContainerPart.PART);
                 if (ModList.get().isLoaded("cyclopscore")) {
                     net.minecraft.client.gui.screens.MenuScreens.register(
                             CyclopsCorePlugin.SCROLLING_CONTAINER_MENU.get(),
@@ -122,8 +133,8 @@ public class JsonMore {
         if (ModList.get().isLoaded("anvil_musbox")) {
             AnvilMusBoxPlugin.load();
         }
-        //if (modList.isLoaded("minecraft")) {
-            MinecraftPlugin.load();
-        //}
+        // if (modList.isLoaded("minecraft")) {
+        MinecraftPlugin.load();
+        // }
     }
 }
